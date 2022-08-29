f47ed34243f60e22674743008d209ab68305986b
csenrique <enrique4848@gmail.com>
2022-08-29 18:46:10 -0400

    CRUD completo Librería Crear, Leer, Actualizar y Borrar ... en base de datos MySQL con Python Django

---
+++ libreria/models.py
@@ -0,0 +1,17 @@
+from distutils.command.upload import upload
+from django.db import models
+
+# Create your models here.
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
+    descripcion = models.TextField(null=True, verbose_name='Descripción')
+    
+    def __str__(self):
+        fila = "Título: " + self.titulo + " - " + "Descripción: " + self.descripcion
+        return fila
+
+    def delete(self, using=None, kepp_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
